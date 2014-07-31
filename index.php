9525f806196ffe359a0344688b58c3f8e1a52124
Michael Davis <askmikeyd@gmail.com>
2014-07-31 11:47:41 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta name="verify-v1" content="NqL/TA3G+AXNmzL8YDSecIJzBT9vAyAShu6dO4Mjqkg=" />
+<meta name="google-site-verification" content="DxEQn-7mjO04JCgFjsQzvu9IjRPIxnQzagqWlUkj5sk" />
+</head>
+<body style="margin:0pt; padding:0pt;">
+<table width="100%" height="100%" bgcolor="#000">
+    <tr>
+        <th><div style="color:#FFF"><h1>Git Example</h1><h1>Coming Soon</h1></div></th>
+    </tr>
+</table>
+</body>
+</html>
